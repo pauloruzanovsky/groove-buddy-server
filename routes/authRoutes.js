37a832d72272c0c06f5e5b2d982401f0a5c76d5e
Paulo Ruzanovsky <pauloruzanovsky@gmail.com>
2023-07-04 08:33:03 -0300

    fix auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,21 +5,21 @@ import passport from 'passport';
 const authRouter = express.Router();
 
 
-authRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email'], failureFlash: true}));
+authRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email']}));
 authRouter.get('/google/callback', passport.authenticate('google'), (req, res) => {
     try {
         res.redirect('https://groove-buddy-server.cyclic.app/');
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
     }
 })
 
-authRouter.get('/github', passport.authenticate('github', { scope: ['profile', 'email'], failureFlash: true }));
+authRouter.get('/github', passport.authenticate('github', { scope: ['profile', 'email'] }));
 authRouter.get('/github/callback', passport.authenticate('github'), (req, res) => {
     try {
         res.redirect('https://groove-buddy-server.cyclic.app/');
     } catch (error) {
-        console.log(error);
+        console.log(error.message);
     }
 })
 
