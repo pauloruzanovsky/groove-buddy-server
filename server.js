d389a87308bffb73aaa3ec4caecbdfef132cef71
Paulo Ruzanovsky <pauloruzanovsky@gmail.com>
2023-07-04 09:52:02 -0300

    fix auth

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ import spotifyApi from './auths/spotifyAuth.js';
 import mongoose from 'mongoose';
 dotenv.config({ path: './config/config.env' });
 
-const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(process.env.MONGODB_URI);
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  }
+// const connectDB = async () => {
+//     try {
+//       const conn = await mongoose.connect(process.env.MONGODB_URI);
+//       console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch (error) {
+//       console.log(error);
+//       process.exit(1);
+//     }
+//   }
 
 const app = express();
 app.use(cors( {
@@ -29,12 +29,12 @@ app.use(cors( {
 app.use(express.json());
 app.enable("trust proxy");
 
-googleSetup(app);
-githubSetup(app)
+// googleSetup(app);
+// githubSetup(app)
 
-app.use('/auth', authRouter);
-app.use('/playlists', playlistRouter);
-app.use('/spotify', spotifyRouter);
+// app.use('/auth', authRouter);
+// app.use('/playlists', playlistRouter);
+// app.use('/spotify', spotifyRouter);
 
 app.get('/getuser', (req, res) => {
     res.send(req.user);
@@ -44,19 +44,19 @@ app.get('/', (req, res) => {
     res.send('hello!')
 })
 
-spotifyApi.clientCredentialsGrant()
-.then((data) => {
-spotifyApi.setAccessToken(data.body['access_token']);
-const songName = 'num'
-spotifyApi.searchTracks(songName, {limit: 1}).then((data) => {
-    const songsArray = data.body.tracks.items.map(song => ({name:song.name, artist:song.artists[0].name,  }))
-    console.log(data.body.tracks.items[0].id)
-}).catch((err) => {
-console.log(err);
-})})
+// spotifyApi.clientCredentialsGrant()
+// .then((data) => {
+// spotifyApi.setAccessToken(data.body['access_token']);
+// const songName = 'num'
+// spotifyApi.searchTracks(songName, {limit: 1}).then((data) => {
+//     const songsArray = data.body.tracks.items.map(song => ({name:song.name, artist:song.artists[0].name,  }))
+//     console.log('a: ',data.body.tracks.items[0].id)
+// }).catch((err) => {
+// console.log(err);
+// })})
   
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-connectDB().then(() => {
-    app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-})
+// connectDB().then(() => {
+    app.listen(PORT, console.log(`Server running on port ${PORT}`));
+// })
